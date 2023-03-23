f2eb495bf377587cbc72e66a321eb8a1ea118216
sfandres94 <andres.sanchez1994@gmail.com>
2023-03-23 18:01:25 +0100

    Update srun_simple.sh

--- CD22_23-P06_single_job/srun_simple.sh
+++ CD22_23-P06_single_job/srun_simple.sh
@@ -2,8 +2,8 @@
 
 echo ""
 echo "Option 1 launched"
-srun -p cpu12c -N 1 -n 1 -t 00:10:00 -o max_fibonacci.out python3 max_fibonacci.py
+srun -p cpu12c -N 1 -n 1 -J max_fibonacci -t 00:10:00 -o max_fibonacci.out python3 max_fibonacci.py
 
 echo ""
 echo "Option 2 launched"
-srun --partition=cpu12c --nodes=1 --ntasks=1 --time=00:10:00 --output=max_fibonacci.out python3 max_fibonacci.py
+srun --partition=cpu12c --nodes=1 --ntasks=1 --job-name=max_fibonacci --time=00:10:00 --output=max_fibonacci.out python3 max_fibonacci.py
